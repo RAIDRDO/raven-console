5bcbaae0ed6493f17b0bc3eebbdd74a80dd4093a
poipiii <nanosonico1@gmail.com>
2022-10-18 20:29:46 +0800

    added base console design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+  useEffect(()=>{console.error("rendered second")},[])
   return (
     <div className="App">
 <button
