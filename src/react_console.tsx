2a17b0fec46c9db04637e1087d389ba18cf46289
poipiii <nanosonico1@gmail.com>
2022-10-18 11:42:07 +0800

    initial commit

---
+++ src/react_console.tsx
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+ 
+const Console = () => {
+    const [savedLogs, setsavedLogs] = useState([]);
+    // let savedLogs:[string] = ["test"];
+    let curry = f => {
+    let out = log => {
+        setsavedLogs([...savedLogs,log])
+        f(log);
+    }
+    return out}
+    console.log = curry(console.log)
+    return (
+        <div>
+            {savedLogs.map((log:string,index:number)=>(
+               <p key={index}>{log}</p> 
+            ))}
+
+        </div>
+        
+    );
+}
+ 
+export default Console;
